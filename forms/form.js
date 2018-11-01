8171894cb5d4fd9d77fd7cc7abb7cd7f904300d5
Kedar <kedarkodgire.kk@gmail.com>
2018-11-01 18:10:24 +0530

    first commit

---
+++ forms/form.js
@@ -0,0 +1,24 @@
+/*$(document).ready(function(){
+  $('#box1').show();
+  $('#box2').hide();
+  $('#box3').hide();
+  $('#box4').hide();
+  $('#box5').hide();
+  var count=1;
+  $('#next').click(function() {
+    /* Act on the event */
+  /*  count+=1;
+    if (count>5) {
+        count=1;
+    }
+    for(var i=1;i<=5;i++){
+      if(i==count){
+        $('#box' + i).show();
+      }else{
+        $('#box'+i).hide();
+      }
+    }
+
+  });
+});
+*/
